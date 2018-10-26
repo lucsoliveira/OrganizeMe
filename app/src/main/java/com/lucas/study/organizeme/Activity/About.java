eaff08f0c5775c431a6e8779f59dd3b89c0c0e1f
Lucas de Oliveira Pereira <olucas1998@gmail.com>
2018-10-26 08:36:53 -0300

    Pequenas Correções

--- app/src/main/java/com/lucas/study/organizeme/Activity/About.java
+++ app/src/main/java/com/lucas/study/organizeme/Activity/About.java
@@ -16,15 +16,13 @@ public class About extends AppCompatActivity {
         View aboutPage = new AboutPage(this)
                 .isRTL(false)
                 .setImage(R.drawable.ic_happy)
-                .addGroup("Connect with us")
-                .addEmail("elmehdi.sakout@gmail.com")
+                .addGroup("Contato")
+                .addEmail("olucas1998@gmail.com")
                 .addWebsite("http://medyo.github.io/")
-                .addFacebook("the.medy")
-                .addTwitter("medyo80")
-                .addYoutube("UCdPQtdWIsg7_pi4mrRu46vA")
+                .addFacebook("olucaoo")
                 .addPlayStore("com.ideashower.readitlater.pro")
-                .addGitHub("medyo")
-                .addInstagram("medyo80")
+                .addGitHub("lucsoliveira")
+                .addYoutube("UCu6PmVEyi_Ea_h9rk2RV7aA")
                 .create();
 
         setContentView(aboutPage);
